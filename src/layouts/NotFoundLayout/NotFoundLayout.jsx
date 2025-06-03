3c17deed8cdf4c36d3be43a5f6e8ee5751827dc9
Daniil Doroshenko <doroshenkowin@gmail.com>
2025-06-03 16:04:22 +0300

    Remove Header From 404Page

--- src/layouts/NotFoundLayout/NotFoundLayout.jsx
+++ src/layouts/NotFoundLayout/NotFoundLayout.jsx
@@ -1,11 +1,10 @@
-import Header from '../MainLayout/sections/Header'
+
 import './NotFoundLayout.css'
 import Footer from './sections/Footer'
 
 export default function NotFoundLayout({ children }) {
 	return (
 		<main className='MainLayout'>
-			<Header />
 			<div className='inner'>{children}</div>
 			<Footer />
 		</main>
